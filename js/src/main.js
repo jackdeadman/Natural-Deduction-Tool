83a2ac9e8a3714ab2fca5fea5bc1d52e4f63c45b
Jack Deadman <jackdeadman@ymail.com>
2016-02-14 20:59:16 +0000

    Update main.js

--- js/src/main.js
+++ js/src/main.js
@@ -1,7 +1,5 @@
 (function(){
     
-    // alert("You are the 10 millionth visitor! You win a prize!");
-    
     var mainWrapper = document.getElementsByClassName('main-wrapper');
     var premiseInput = document.getElementById('premise-input');
     var conclusionInput = document.getElementById('conclusion-input');
